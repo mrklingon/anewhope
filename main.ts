a28997c20d8b107feac8e59f9ebd7f5db119d871
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2023-06-13 18:05:23 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,39 +1,65 @@
-let Tantavie = images.createBigImage(`
-    . . . . . . . . . .
-    . . . . . # . . . .
-    . . . . . # # # # #
-    . . . . . . . . . .
-    . . . . . . . . . .
-    `)
-let destroyer = images.createBigImage(`
-    # # . . . . . . . .
-    # # # # # . . . . .
-    # # # # # # # . . .
-    . . . . . . . . . .
-    . . . . . . . . . .
-    `)
-let blank = images.createBigImage(`
-    . . . . . . . . . .
-    . . . . . . . . . .
-    . . . . . . . . . .
-    . . . . . . . . . .
-    . . . . . . . . . .
-    `)
-Tantavie.scrollImage(-1, 200)
-destroyer.scrollImage(-1, 200)
-blank.scrollImage(-1, 200)
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . . . .
-    . # # # .
-    # # # # #
-    `)
-for (let index = 0; index <= 2; index++) {
-    led.plot(index, index)
-    basic.pause(200)
-    led.unplot(index, index)
+function flight () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . # # # .
+        # # # # #
+        `)
+    for (let index = 0; index <= 2; index++) {
+        led.plot(index + 2, 2 - index)
+        basic.pause(200)
+        led.unplot(index + 2, 2 - index)
+    }
+    images.createImage(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `).scrollImage(1, 200)
 }
-basic.forever(function () {
-	
+function landing () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . # # # .
+        # # # # #
+        `)
+    for (let index = 0; index <= 2; index++) {
+        led.plot(index, index)
+        basic.pause(200)
+        led.unplot(index, index)
+    }
+    basic.pause(500)
+    images.createImage(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `).scrollImage(1, 200)
+}
+function scene1 () {
+    images.createBigImage(`
+        . . . . . . # # # #
+        . . . . # # # # # #
+        # # # . . . . . . .
+        # . . . . . . . . .
+        . . . . . . . . . .
+        `).scrollImage(1, 200)
+    images.createImage(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `).scrollImage(1, 200)
+    landing()
+}
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    flight()
 })
+scene1()
+let state = 0
